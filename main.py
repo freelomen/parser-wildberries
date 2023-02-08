374daf97b281cde6b1510948a9dde78046162e57
Andrei1999 <kent.mihailov1999@mail.ru>
2023-02-08 13:27:06 +0500

    write items url list json

--- main.py
+++ main.py
@@ -1,65 +1,86 @@
+import json
+import os
+
 import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-def get_data(url):
+def isExistNextPage(driver):
+    try:
+        elem = driver.find_element(By.CLASS_NAME, 'pagination'). \
+            find_element(By.CLASS_NAME, 'pagination__wrapper'). \
+            find_element(By.CLASS_NAME, 'pagination__next')
+        return True
+    except NoSuchElementException:
+        return False
+
+
+def get_data_page(url):
+    print("Подключение установлено")
     options = webdriver.ChromeOptions()
-    options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
+    options.add_argument(
+        f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
     options.add_argument("--disable-blink-features=AutomationControlled")
     # options.add_argument("--headless")
-
     service = Service('D:\\PythonProjects\\parser-wildberries\\chromedriver.exe')
-
     driver = webdriver.Chrome(service=service, options=options)
-    driver.maximize_window()
+    # driver.maximize_window()
 
     try:
+        current_page_number = 1
         driver.get(url=url)
-        print("Подключение установлено")
+        time.sleep(5)
 
-        print("Ожмдание 10 секунд")
-        time.sleep(30)
-
-        print("Пролистываем вниз")
-        # target = driver.find_element(By.CLASS_NAME, "pagination")
-        # actions = ActionChains(driver)
-        # actions.move_to_element(target)
-        # actions.perform()
-
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        SCROLL_PAUSE_TIME = 1
-
+        SCROLL_PAUSE_TIME = 2
         # Get scroll height
         last_height = driver.execute_script("return document.body.scrollHeight")
-
         while True:
             # Scroll down to bottom
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
             # Wait to load page
             time.sleep(SCROLL_PAUSE_TIME)
-
             # Calculate new scroll height and compare with last scroll height
             new_height = driver.execute_script("return document.body.scrollHeight")
             if new_height == last_height:
                 break
             last_height = new_height
 
-        print("Ожмдание 10 секунд")
-        time.sleep(10)
-
-        print("Сохранение страницы")
-        with open("index.html", "w", encoding="utf-8") as file:
+        time.sleep(5)
+        print(f"Сохраняем страницу: {current_page_number}")
+        with open(f"data_page/index-{current_page_number}.html", "w", encoding="utf-8") as file:
             file.write(driver.page_source)
 
-        print("Ожмдание 10 секунд")
-        time.sleep(10)
+        while (isExistNextPage(driver=driver)):
+            current_page_number += 1
+            driver.get(url=f"{url}?sort=popular&page={current_page_number}")
+            time.sleep(5)
+
+            SCROLL_PAUSE_TIME = 2
+            # Get scroll height
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            while True:
+                # Scroll down to bottom
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Wait to load page
+                time.sleep(SCROLL_PAUSE_TIME)
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+
+            time.sleep(5)
+            print(f"Сохраняем страницу: {current_page_number}")
+            with open(f"data_page/index-{current_page_number}.html", "w", encoding="utf-8") as file:
+                file.write(driver.page_source)
+
+            print(f"Страницы сохранены")
     except Exception as ex:
         print(ex)
     finally:
@@ -69,20 +90,36 @@ def get_data(url):
         print("Закрытие драйвера")
         driver.quit()
 
-    print("Открываем страницу для чтения")
-    with open("index.html", encoding="utf-8") as file:
-        src = file.read()
 
-    soup = BeautifulSoup(src, 'lxml')
-    item_cards = soup.find_all("div", class_="product-card j-card-item")
+def get_item_url():
+    current_page_number = 1
+    current_item_number = 1
+    item_url_list = []
 
-    for item_url in item_cards:
-        item_url = f'{item_url.find("a").get("href")}'
-        print(item_url)
+    for filename in os.listdir("data_page"):
+        print(f"Собираем ссылки: {current_page_number}")
+        with open(os.path.join("data_page", filename), 'r', encoding="utf-8") as file:
+            src = file.read()
+        soup = BeautifulSoup(src, 'lxml')
+        item_cards = soup.find_all("div", class_="product-card j-card-item")
+        for item_url in item_cards:
+            item_url = f'{item_url.find("a").get("href")}'
+            item_url_list.append(item_url)
+            # print(f"Товар: {current_item_number}. Ссылка: {item_url}")
+            current_item_number += 1
+        current_page_number += 1
+
+    with open('item_url_list.json', 'w') as file:
+        json.dump(item_url_list, file, indent=4, ensure_ascii=False)
+
 
 def main():
-    # get_data("https://www.wildberries.ru/catalog/elektronika/smartfony-i-telefony/vse-smartfony")
-    get_data("https://www.wildberries.ru/catalog/aksessuary/veera")
+    # Сохраняем каталог товаров
+    # get_data_page("https://www.wildberries.ru/catalog/elektronika/smartfony-i-telefony/vse-smartfony")
+    get_data_page("https://www.wildberries.ru/catalog/aksessuary/veera")
+
+    # Выдергиваем ссылки карточек товаров
+    get_item_url()
 
 
 if __name__ == '__main__':
